cb574308f3e9910bd6717774c66a19c3037b2c37
Roberto <vecchioroberto93@gmail.com>
2021-01-07 12:29:55 +0100

    Handler

--- src/com/robertovecchio/model/user/Handler.java
+++ src/com/robertovecchio/model/user/Handler.java
@@ -3,17 +3,37 @@ package com.robertovecchio.model.user;
 import java.time.LocalDate;
 
 /**
- * Questa classe rappresenta uno degli attori del sistema software Taxi finder*/
+ * Questa classe rappresenta uno degli attori del sistema software Taxi finder e si occupa di astrarre il concetto
+ * di gestore aziendale
+ * @author robertovecchio
+ * @version 1.0
+ * @since 7/01/2021
+ * */
 public class Handler extends UserAccount{
 
     //==================================================
     //                   Costruttori
     //==================================================
+
+    /**
+     * Costruttore di un Gestore
+     * @param fiscalCode codice fiscale utente
+     * @param firstName nome utente
+     * @param lastName cognome utente
+     * @param dateOfBirth data di nascita utente
+     * @param genderType genere sessuale utente
+     * @param email email utente
+     * @param username username utente
+     * @param password password utente
+     * @see LocalDate
+     * @see GenderType
+     * */
     public Handler(String fiscalCode, String firstName,
                    String lastName, LocalDate dateOfBirth,
                    GenderType genderType, String email,
                    String username, String password){
 
+        // Richiamo il costruttore della classe astratta UserAccount
         super(fiscalCode, firstName, lastName, dateOfBirth, genderType, email, username, password);
     }
 }
