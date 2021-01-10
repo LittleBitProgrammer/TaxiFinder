1515912d20327728331a8dc4c7c6a93875a14219
Roberto <vecchioroberto93@gmail.com>
2021-01-10 18:32:26 +0100

    Controller creation and fxml creation

---
+++ src/com/robertovecchio/controller/dialog/AddWaitingStationController.java
@@ -0,0 +1,10 @@
+package com.robertovecchio.controller.dialog;
+
+/**
+ * Classe che gestisce la view di aggiunta postazione
+ * @author robertovecchio
+ * @version 1.0
+ * @since 10/01/2021
+ * */
+public class AddWaitingStationController {
+}
---
+++ src/com/robertovecchio/controller/dialog/RemoveTaxiDriverController.java
@@ -0,0 +1,10 @@
+package com.robertovecchio.controller.dialog;
+
+/**
+ * Classe che gestisce la view di rimozione Tassista
+ * @author robertovecchio
+ * @version 1.0
+ * @since 10/01/2021
+ * */
+public class RemoveTaxiDriverController {
+}
